37289ac0739f9322f59b2f9f062cf7f197b59aeb
Lakshya Khera <lakshya.khera@gmail.com>
2020-04-29 01:46:08 +0530

    Added Lifecycle methods on mainactivity 🚀

--- app/src/main/java/com/example/android_stew/MainActivity.java
+++ app/src/main/java/com/example/android_stew/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android_stew;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView para = findViewById(R.id.lifecycle);
+        para.setText("Hello there, This is opening page of android stew! So First we'll look over lifecycle methods in android.\n\nThese are functions which are execute accordingly on certain events! Like onCreate,onStop etc!.\n Open log and have a look!");
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
     }
 
 }
